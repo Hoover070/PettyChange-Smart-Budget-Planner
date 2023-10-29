67e81578c082d46b34d7ce548d3904e8ce04b81e
Will Hoover <willhooverfilm@gmail.com>
2023-10-29 12:43:47 -0400

    updated pages, encased the model in the project,

--- RandD_smartPlanner/Budget.cs
+++ RandD_smartPlanner/Budget.cs
@@ -3,7 +3,8 @@ namespace RandD_smartPlanner
 {
 
     public class Budget
-    {
+    {   
+        public string BudgetName { get; set; }
         public double Income { get; set; }
         public double Expenses { get; set; }
         public double SavingsGoal { get; set; }
@@ -11,6 +12,8 @@ namespace RandD_smartPlanner
         public double AISuggestedSavings { get; set; }
         public double AISuggestedTimeframe { get; set; }
         private User currentUser;
+        public double IncomeDiff { get; set; }
+        public double minSavingsLimit { get; set; }
 
     }
 
